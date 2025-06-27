5225b94a4c1332c4b0e93090ee8d81c94ed830fb
MicaelCosmo <micael.trabalho@hotmail.com>
2025-06-26 22:19:01 -0300

    feat: 'If and Else' Condições

---
+++ aula_11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 59.3
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+else {
+    console.log('Você está dentro da velocidade permitida.')
+}
+console.log('Dirija sempre usando cinto de segurança!')

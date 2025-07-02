d244b58f79a4d7fdf9792064960e1ff31ccc49dd
MicaelCosmo <micael.trabalho@hotmail.com>
2025-07-02 18:13:20 -0300

    feat: Exercício de tabuada

--- aula_14ex/ex017/modelo/script.js
+++ aula_14ex/ex017/modelo/script.js
@@ -1,1 +1,22 @@
-alert('Olá!')
+function gerarTabuada() {
+    let num = document.getElementById('txtn').value;
+    let tab = document.getElementById('seltab');
+
+    if (num.length == 0) {
+        window.alert('Por favor, digite um número!');
+    } else {
+        let n = Number(num);
+        let c = 1;
+        tab.innerHTML = ''; // Limpa o conteúdo anterior da tabela
+
+        while (c <= 10) {
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n * c}`;
+            tab.value = `tab${c}`; // Define o valor do item
+            tab.appendChild(item);
+            c++;
+        }
+    }
+
+
+}
